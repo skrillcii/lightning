94fde0bafa594e3ae50e7a32d42a88eb1129f6d0
skrillcii <skrillcii.github.com>
2020-11-17 23:03:15 +0900

    enable multi-gpus training

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from model import LitAutoEncoder
 from torchvision.datasets import MNIST
 from torchvision import transforms
 from torch.utils.data import DataLoader
-from torch.utils.data import random_split
 import pytorch_lightning as pl
-
 
 dataset = MNIST(os.getcwd() + '/datasets', download=True,
                 transform=transforms.ToTensor())
-train_loader = DataLoader(dataset)
+train_loader = DataLoader(dataset, batch_size=60000, num_workers=4)
 
 # init model
 autoencoder = LitAutoEncoder()
@@ -17,5 +15,16 @@ autoencoder = LitAutoEncoder()
 # most basic trainer, uses good defaults
 # (auto-tensorboard, checkpoints, logs, and more)
 # trainer = pl.Trainer(gpus=8) (if you have GPUs)
-trainer = pl.Trainer()
+trainer = pl.Trainer(gpus=[0, 1], num_nodes=1, accelerator='dp', max_epochs=50)
 trainer.fit(autoencoder, train_loader)
+
+# train on TPUs using 16 bit precision
+# using only half the training data and checking
+# validation every quarter of a training epoch
+#
+# trainer = pl.Trainer(
+#         tpu_cores=8,
+#         precision=16,
+#         limit_train_batches=0.5,
+#         val_check_interval=0.25
+# )
